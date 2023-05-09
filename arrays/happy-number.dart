356e4e7370db4c4e691afa9eefb5a1d1ffd7e254
1712916 <kieuphong2840@gmail.com>
2023-05-09 21:11:16 +0700

    array: https://leetcode.com/problems/happy-number

---
+++ arrays/happy-number.dart
@@ -0,0 +1,62 @@
+/*
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+ */
+
+import '../execute_time.dart';
+
+void main() {
+  measureExecutionTime(() {
+    print(Solution().isHappy(19)); //true
+    print(Solution().isHappy(7)); //true
+    print(Solution().isHappy(1111111)); //true
+    print(Solution().isHappy(2)); //false
+  });
+}
+
+class Solution {
+  bool isHappy(int n) {
+    if (n < 10) {
+      // trường hợp đặc biệt
+      return n == 1 || n == 7;
+    }
+
+    int total = 0;
+
+    while (n > 0) {
+      total += (n % 10) * (n % 10);
+
+      //gán phần nguyên
+      n = n ~/ 10;
+    }
+
+    return isHappy(total);
+  }
+}
